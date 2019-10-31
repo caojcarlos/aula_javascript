7091e4e04c66dacd4589781e636a6e858209d6d6
Jose Carlos Nunes <caojcarlos0807@gmail.com>
2019-10-31 19:53:13 -0300

    Atualização do Main.js utilzando condicional, lacos e trabalahando co  date

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-var nome = "Jose Carlos Nunes";
+/*var nome = "Jose Carlos Nunes";
 var idade = 44;
 var frase = "Japao é melhor time do mundo";
 //alert("Bem Vindo, "+ nome + " tem "+idade+" anos");
 //coemntario de uma linha
-/*
+
 coemntario de mais de uma
 llinha
-*/
+
 // declarando variaveis numericas,
 var num = 10;
 var num2 = 29;
@@ -22,7 +22,7 @@ console.log(frase.replace("Japao", "Brasil"));
 console.log(frase.toUpperCase());
 console.log(frase.toLowerCase());
 
-/*Criação de lista e publicaocao pelo console */
+//Criação de lista e publicaocao pelo console 
 var lista = ["Maça", "Pera", "Laranja"];
 
 console.log(lista);
@@ -68,7 +68,59 @@ var frutas = [{nome:"maça", cor:"vermelho"}, {nome:"laranja", cor:"laranja"}, {
 console.log(frutas);
 //exisme somente um item do dicionando dentro da lista
 console.log(frutas[1].nome);
+*/
 
+
+//criando if para validação
+var idade = 18;
+/*
+if(idade >= 18){
+    alert("Maior de Idade");
+}else{
+    alert("Menor de idade");
+
+}
+*/
+// utilizando o prompt para receber a informação pelo usuario
+
+//var idade = prompt("Qaula a sua Idade? ");
+if(idade >= 18){
+    alert("Maior de Idade");
+}else{
+    alert("Menor de idade");
+
+}
+
+//laço de repetição while
+var count = 0;
+while( count <= 5){
+    console.log(count)
+    count=count + 1 ;
+}
+
+//laco de repeticao for
+var cotando;
+for(cotando = 1; cotando <= 5 ;cotando++){
+    console.log(cotando)
+}
+
+//trabalando com data
+//declarando uma variavel contendo data
+
+var d = new Date();
+console.log(d);
+//mostra o dia da semana
+console.log(d.getDay());
+//dia do mes
+console.log(d.getUTCDate());
+//mostra mes
+console.log(d.getMonth()+1);
+//mostra ano
+console.log(d.getFullYear());
+//mostra hora
+console.log(d.getHours());
+//mostra minutos
+console.log(d.getMinutes());
 
 
 
