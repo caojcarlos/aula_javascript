f80ecea83efbc2608c7f7fbb8d4893f23d095fc0
Jose Carlos Nunes <caojcarlos0807@gmail.com>
2019-10-31 18:51:43 -0300

    Meu Index e meus Primeiros comandos em JS

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var nome = "Jose Carlos Nunes";
+var idade = 44;
+var frase = "Japao é melhor time do mundo";
+//alert("Bem Vindo, "+ nome + " tem "+idade+" anos");
+//coemntario de uma linha
+/*
+coemntario de mais de uma
+llinha
+*/
+// declarando variaveis numericas,
+var num = 10;
+var num2 = 29;
+
+//alert("soma: " + (num + num2) + "\nsubtracao: " + (num2 - num) + "\ndivisao: " + (num2 / num) + "\nmultiplicacao: " + (num * num2));
+console.log ("soma: " + (num + num2) + "\nsubtracao: " + (num2 - num) + "\ndivisao: " + (num2 / num) + "\nmultiplicacao: " + (num * num2));
+
+console.log(frase);
+//troca a palavra por outra
+console.log(frase.replace("Japao", "Brasil"));
+
+//toUpperCase(maiusculas) e tolowerCase(minustuclas
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
