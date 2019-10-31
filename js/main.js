a208cec1f77763c2e45b3b22e302e4066e1543e4
Jose Carlos Nunes <caojcarlos0807@gmail.com>
2019-10-31 19:27:14 -0300

    Atualização do Main.js com criação de listas e dicionarios

--- js/main.js
+++ js/main.js
@@ -21,3 +21,54 @@ console.log(frase.replace("Japao", "Brasil"));
 //toUpperCase(maiusculas) e tolowerCase(minustuclas
 console.log(frase.toUpperCase());
 console.log(frase.toLowerCase());
+
+/*Criação de lista e publicaocao pelo console */
+var lista = ["Maça", "Pera", "Laranja"];
+
+console.log(lista);
+// mostra na lista somente um item especifico
+console.log(lista[1]);
+
+//para adicionar um item a lista
+lista.push("Uva");
+console.log(lista);
+
+//para remever o ultimo item a lista
+lista.pop();
+console.log(lista);
+
+//ver o tamanho da lista
+console.log(lista.length);
+
+
+//ver a lista ao contrario 
+console.log(lista.reverse());
+
+//transforma os itens da lsita em string
+console.log(lista.toString());
+
+//transforma os itens da lsita em string mostrando o primeiro elemento da lista e a primeira string
+
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+
+// deixa a lista como string e adiciona um separado conforme seu desejo
+console.log(lista.join(" - "));
+console.log(lista.join(";"));
+
+// criação de um dicionaod, como utilizamos no json
+var fruta = {nome:"maça", cor:"vermelho"};
+console.log(fruta);
+
+//exibe o teim do dicionario
+console.log(fruta.nome);
+
+// criação de um dicionario com mais itens e converte em lista 
+var frutas = [{nome:"maça", cor:"vermelho"}, {nome:"laranja", cor:"laranja"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+//exisme somente um item do dicionando dentro da lista
+console.log(frutas[1].nome);
+
+
+
+
